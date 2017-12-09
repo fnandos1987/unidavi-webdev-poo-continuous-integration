20fc9149f32ce14a9df79fee62d5197b4542bc66
user <user@user-PC>
2017-12-08 22:07:00 -0200

    Commit Inicial. Classe App em projeto Maven básico

---
+++ src/main/java/br/edu/unidavi/continuousintegration/App.java
@@ -0,0 +1,12 @@
+package br.edu.unidavi.continuousintegration;
+
+/**
+ *
+ * @author fernando.schwambach
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("App");
+    }    
+}
