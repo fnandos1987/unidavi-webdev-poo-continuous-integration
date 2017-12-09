04f86464b8d352e2520315308f2d5ec915b86d0b
user <user@user-PC>
2017-12-09 08:25:24 -0200

    Classe de teste automatizado do exercism

---
+++ src/main/java/br/edu/unidavi/continuousintegration/BookStore.java
@@ -0,0 +1,14 @@
+package br.edu.unidavi.continuousintegration;
+
+import java.util.List;
+
+/**
+ *
+ * @author fernando.schwambach
+ */
+public class BookStore {
+    
+    public Integer calculateBasketCost(List<Integer> books){
+        return 8;        
+    }    
+}
